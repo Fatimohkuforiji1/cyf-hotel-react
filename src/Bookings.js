a8c5001df0503314a74d65ca9151a85ec249866c
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-04-12 08:00:38 +0100

    Got data using useEffect

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,24 +1,54 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Search from "./Search.js";
 import allFiles from "./HotelReact.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState();
    //setBookings
   const search = searchVal => {
+    const searchValCase = searchVal.toLowerCase();
+    const filteredSearch = bookings.filter(function(booking){
+      if ((booking["firstName"].toLowerCase().includes(searchValCase))
+      ||(booking["surname"].toLowerCase().includes(searchValCase))){
+      return booking;
+      }
+    })
+    if(filteredSearch.length != 0){
+      setBookings(filteredSearch);
+    }
     console.info("TO DO!", searchVal);
   };
 
-  return (
+  const  [dataFetch, setDataFetch] =useState(false);
+
+  const DelayFetch =() =>{
+    fetch (`https://cyf-react.glitch.me`)
+    .then(function (response){
+      return response.json()
+     })
+     .then(function(data){
+       setBookings(data)
+       setDataFetch(true);
+     })
+     .catch(function(error){
+      alert("Please refresh, there is an error");
+     })
+  }
+  useEffect (()=>{
+    setTimeout(()=>DelayFetch(), 5000)
+     }, []);
+
+  return dataFetch ? (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
         <allFiles.SearchResults results={bookings} />
       </div>
     </div>
-  );
-};
+  ) :(<h2>Page Loading, Please Wait....  </h2>)
+}
+
 
 export default Bookings;
